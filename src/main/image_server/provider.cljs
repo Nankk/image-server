c2b6540b8a2e29b7e92d2aaa1b826145e0fad9aa
Nankk <toc32768@gmail.com>
2020-04-12 22:59:29 +0900

    :+1: Add get-thumbnail w/o query

--- src/main/image_server/provider.cljs
+++ src/main/image_server/provider.cljs
@@ -27,14 +27,20 @@
 
 ;; Get thumbnail (DONE)
 
-(defn handle-get-thumbnail [req res]
+(defn handle-get-thumbnail
+  "Returns
+  1. thumbnail of the selected image when no query specified
+  2. thumbnail of the image corresponding to the item-idx in the current page specified in the query."
+  [req res]
   (println "handle-get-thumbnail")
   (try
     (let [query      (util/->query req)
           item-idx   (js/parseInt (query :item-idx))
           _          (println "item-idx: " item-idx)
           page-idx   (@db/db :page-idx)
-          target-idx (+ (* (* const/thumb-rows const/thumb-columns) page-idx) item-idx)
+          target-idx (if (not (js/isNaN item-idx))
+                       (+ (* (* const/thumb-rows const/thumb-columns) page-idx) item-idx)
+                       (first (keep-indexed #(when (= (%2 :selected?) true) (println "ki:" %1) %1) (@db/db :img-list))))
           _          (println "calculated target-idx" target-idx)
           target     (get (@db/db :img-list) target-idx)
           path       (if target
